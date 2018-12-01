8f9da9acdfe8c19c746bbfcf9818713f5d1ccfb1
Krystyna Kruchkovska <kruchok883@gmail.com>
2018-12-01 14:14:09 +0200

    Fix relationship

---
+++ TaskCoreDataTableView/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  TaskCoreDataTableView
+//
+//  Created by Mac on 12/1/18.
+//  Copyright © 2018 CO.KrystynaKruchcovska. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class User: NSManagedObject {
+
+}
